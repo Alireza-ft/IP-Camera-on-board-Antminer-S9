cb96ad5166f90d2c5df9759137002a8e6094f45a
Arf <Ar.fatemi28@gmail.com>
2026-07-04 23:16:42 +0430

    Arf/testing iwli librarys

--- ip_camera/ipcamera_udp/src/main.c
+++ ip_camera/ipcamera_udp/src/main.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
+#include "xparameters.h"
+#include "xil_printf.h"
+
+#include "lwip/init.h"
+#include "lwip/ip_addr.h"
+#include "lwip/udp.h"
 
 int main(void)
 {
-    printf("project from new began\n");
-    while (1) {
+    xil_printf("Project Start\r\n");
 
-        }
+    while(1)
+    {
+
+    }
+
+    return 0;
 }
